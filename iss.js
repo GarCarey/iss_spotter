e314958f366596a9eda57bc909c84cba67096788
Gareth <gar.carey@gmail.com>
2021-08-21 19:04:23 -0400

    add fetchCoords function

--- iss.js
+++ iss.js
@@ -20,10 +20,26 @@ const fetchMyIP = function(callback) {
       return;
     }
 
-    const data = JSON.parse(body).ip;
+    const ip = JSON.parse(body).ip;
 
-    callback(null, data);
+    callback(null, ip);
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching longitude and latitude. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    
+    const { latitude, longitude} = JSON.parse(body);
+
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
