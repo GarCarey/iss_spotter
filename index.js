540cdbdc2224734cc6cf703d99c1bef213b17c5f
Gareth <gar.carey@gmail.com>
2021-08-23 19:45:27 -0400

    add nextISSTimes function to to print out all the times of the flyovers

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyoverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
+const printFlyoverTimes = function(passTimes) {
+  for( const pass of passTimes) {
+    const dateTime = new Date(0);
+    dateTime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${dateTime} for ${duration} seconds!`);
   }
+};
 
-  console.log('IP address is: ', ip);
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  //success, print out the deets
+  printFlyoverTimes(passTimes);
 });
 
-fetchCoordsByIP('142.120.88.25', (error, coordinates) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
-  }
-
-  console.log('Coordinates are: ',coordinates);
-});
-
-fetchISSFlyoverTimes({ latitude: '43.7489', longitude: '-79.4422' }, (error, flyovers) => {
-  if (error) {
-    console.log("It didn't work", error);
-    return;
-  }
-
-  console.log('Time of ISS flyovers are: ', flyovers);
-});
+//fetchMyIP((error, ip) => {
+//  if (error) {
+//    nextISSTimesForMyLocation(error, null);
+//  }
+//
+//  console.log('IP address is: ', ip);
+//});
+//
+//fetchCoordsByIP('142.120.88.25', (error, coordinates) => {
+//  if (error) {
+//    nextISSTimesForMyLocation(error, null);
+//  }
+//
+//  console.log('Coordinates are: ',coordinates);
+//});
+//
+//fetchISSFlyoverTimes({ latitude: '43.7489', longitude: '-79.4422' }, (error, flyovers) => {
+//  if (error) {
+//    nextISSTimesForMyLocation(error, null);
+//  }
+//
+//  console.log('Time of ISS flyovers are: ', flyovers);
+//});
