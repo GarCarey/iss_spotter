ebddb186c9c500ef686bed0b3441481c044d1198
Gareth <gar.carey@gmail.com>
2021-08-23 19:03:07 -0400

    add fetchISSFlyoverTimes function

--- iss.js
+++ iss.js
@@ -42,4 +42,21 @@ const fetchCoordsByIP = function(ip, callback) {
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordsByIP };
+const fetchISSFlyoverTimes = function({ latitude, longitude }, callback) {
+  request(`http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`, (error, response, body) => {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching flyover times. Response: ${body}`;
+      callback(Error(msg), null);
+      return; 
+    }
+
+    const flyovers = JSON.parse(body).response;
+
+    callback(null, flyovers);
+
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyoverTimes };
